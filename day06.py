f2994237eaf86edddd8d8bfc1bc0b36aae7099c0
scottdharris <scottdharris11@yahoo.com>
2024-12-06 11:16:35 -0500

    day 6, part 2 wrong answer (too low)

--- day06.py
+++ day06.py
@@ -42,7 +42,7 @@ def solve_part2(grid: list[str]):
     visited = set()
     visited.add(loc)
     visited_in_dir = set()
-    loop_points = set()
+    loop_points = 0
     while True:
         visited_in_dir.add(loc + directions[d])
         if turn_into_loop(grid, loc, d, visited_in_dir):
@@ -68,7 +68,10 @@ def find_guard(grid: list[str]) -> Tuple[int, int]:
                 return ((x, y))
 
 def turn_into_loop(grid: list[str], loc: Tuple[int,int], d: int, vid: Set) -> bool:
-    """determine if putting a turn at the path point ahead would trigger loop"""
+    """
+    determine if putting a turn at the path point ahead would trigger loop
+    by checking to see if a path in that direction has already been followed.
+    """
     height = len(grid)
     width = len(grid[0])
     d += 1
