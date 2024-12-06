721d604797f09ac1cd0708a2579e630667e12cba
scottdharris <scottdharris11@yahoo.com>
2024-12-06 16:53:23 -0500

    day 6, part 2 still wrong clean up

--- day06.py
+++ day06.py
@@ -11,23 +11,17 @@ def solve_part1(grid: list[str]):
     loc = find_guard(grid)
     height = len(grid)
     width = len(grid[0])
-    
+
     d = 0
     visited = set()
     visited.add(loc)
-    visited_in_dir = set()
     while True:
-        visited_in_dir.add(loc + directions[d])
-        loc = (loc[0]+directions[d][0],loc[1]+directions[d][1])
-        if loc[0] < 0 or loc[0] >= width:
-            break
-        if loc[1] < 0 or loc[1] >= height:
+        loc = tuple(x + y for x, y in zip(loc, directions[d]))
+        if loc[0] < 0 or loc[0] >= width or loc[1] < 0 or loc[1] >= height:
             break
         if grid[loc[1]][loc[0]] == '#':
-            loc = (loc[0]-directions[d][0],loc[1]-directions[d][1])
-            d += 1
-            if d >= len(directions):
-                d = 0
+            loc = tuple(x - y for x, y in zip(loc, directions[d]))
+            d = turn(d)
         visited.add(loc)
     return len(visited)
 
@@ -47,16 +41,12 @@ def solve_part2(grid: list[str]):
         visited_in_dir.add(loc + directions[d])
         if turn_into_loop(grid, loc, d, visited_in_dir, visited):
             loop_points += 1
-        loc = (loc[0]+directions[d][0],loc[1]+directions[d][1])
-        if loc[0] < 0 or loc[0] >= width:
-            break
-        if loc[1] < 0 or loc[1] >= height:
+        loc = tuple(x + y for x, y in zip(loc, directions[d]))
+        if loc[0] < 0 or loc[0] >= width or loc[1] < 0 or loc[1] >= height:
             break
         if grid[loc[1]][loc[0]] == '#':
-            loc = (loc[0]-directions[d][0],loc[1]-directions[d][1])
-            d += 1
-            if d >= len(directions):
-                d = 0
+            loc = tuple(x - y for x, y in zip(loc, directions[d]))
+            d = turn(d)
         visited.add(loc)
     return loop_points
 
@@ -67,6 +57,13 @@ def find_guard(grid: list[str]) -> Tuple[int, int]:
             if col == '^':
                 return ((x, y))
 
+def turn(current: int) -> int:
+    """turn direction"""
+    d = current + 1
+    if d >= len(directions):
+        d = 0
+    return d
+
 def turn_into_loop(grid: list[str], loc: Tuple[int,int], d: int, vid: Set, visited: Set) -> bool:
     """
     determine if putting a turn at the path point ahead would trigger loop
@@ -74,39 +71,33 @@ def turn_into_loop(grid: list[str], loc: Tuple[int,int], d: int, vid: Set, visit
     """
     height = len(grid)
     width = len(grid[0])
-    place_point = (loc[0]+directions[d][0],loc[1]+directions[d][1])
-    
+    point = tuple(x + y for x, y in zip(loc, directions[d]))
+
     # outside grid
-    if place_point[0] < 0 or place_point[0] >= width:
-        return False
-    if place_point[1] < 0 or place_point[1] >= height:
+    if point[0] < 0 or point[0] >= width or point[1] < 0 or point[1] >= height:
         return False
     # already something there
-    if grid[place_point[1]][place_point[0]] in ['#','^']:
+    if grid[point[1]][point[0]] in ['#','^']:
         return False
     # visited on path already so not eligible since it would change the path to get here
-    if place_point in visited:
+    if point in visited:
         return False
 
+    # turn and then process until either getting out (no loop)
+    # or a path already used is entered (indicating loop)
+    d = turn(d)
     vid = vid.copy()
-    d += 1
-    if d >= len(directions):
-        d = 0
     while True:
-        loc = (loc[0]+directions[d][0],loc[1]+directions[d][1])
-        if loc[0] < 0 or loc[0] >= width:
-            return False
-        if loc[1] < 0 or loc[1] >= height:
+        loc = tuple(x + y for x, y in zip(loc, directions[d]))
+        if loc[0] < 0 or loc[0] >= width or loc[1] < 0 or loc[1] >= height:
             return False
         if grid[loc[1]][loc[0]] == '#':
-            loc = (loc[0]-directions[d][0],loc[1]-directions[d][1])
-            d += 1
-            if d >= len(directions):
-                d = 0
+            loc = tuple(x - y for x, y in zip(loc, directions[d]))
+            d = turn(d)
         loc_in_dir = loc + directions[d]
         if loc_in_dir in vid:
             return True
-        vid.add(loc + directions[d])
+        vid.add(loc_in_dir)
 
 # Data
 data = read_lines("input/day06/input.txt")
