77fd37a107bc6aeb4cc062ec71123d0519345cf1
scottdharris <scottdharris11@yahoo.com>
2024-12-07 06:24:20 -0500

    day 7, part 2

--- day07.py
+++ day07.py
@@ -9,14 +9,19 @@ def solve_part1(lines: list[str]):
     valid = 0
     for line in lines:
         test, values = parse(line)
-        if equation_valid(0, test, values):
+        if equation_valid(0, test, values, ['+', '*'], []):
             valid += test
     return valid
 
 @runner("Day 7", "Part 2")
 def solve_part2(lines: list[str]):
     """part 2 solving function"""
-    return 0
+    valid = 0
+    for line in lines:
+        test, values = parse(line)
+        if equation_valid(0, test, values, ['+', '*', '|'], []):
+            valid += test
+    return valid
 
 def parse(line: str) -> Tuple[int, list[int]]:
     """parse input line"""
@@ -25,17 +30,24 @@ def parse(line: str) -> Tuple[int, list[int]]:
     values = list(map(int, line[idx+1:].strip().split(" ")))
     return test, values
 
-def equation_valid(start: int, test: int, values: list[int]) -> bool:
+def equation_valid(start: int, test: int, values: list[int], ops: list[chr], prev: list) -> bool:
     """recursively compute until complete and validate against test"""
+    if len(values) == 0:
+        return start == test
     val = values[0]
-    if len(values) == 1:
-        return (start + val) == test or (start * val) == test
-    else:
-        if equation_valid(start + val, test, values[1:]):
-            return True
-        if equation_valid(start * val, test, values[1:]):
-            return True
-        return False
+    for op in ops:
+        if op == '+':
+            if equation_valid(start + val, test, values[1:], ops, prev + [op, val]):
+                return True
+        elif op == '*' and start > 0:
+            if equation_valid(start * val, test, values[1:], ops, prev + [op, val]):
+                return True
+        elif op == '|':
+            if start == 0:
+                continue
+            if equation_valid(int(str(start)+str(val)), test, values[1:], ops, prev + [op, val]):
+                return True
+    return False
 
 # Data
 data = read_lines("input/day07/input.txt")
@@ -54,5 +66,5 @@ assert solve_part1(sample) == 3749
 assert solve_part1(data) == 6083020304036
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample) == 11387
+assert solve_part2(data) == 59002246504791
