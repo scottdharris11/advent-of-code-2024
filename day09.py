b4bda5fdf1380336058b91d8026587088673d124
scottdharris <scottdharris11@yahoo.com>
2024-12-09 17:15:46 -0500

    improve perf, array offsets

--- day09.py
+++ day09.py
@@ -53,24 +53,28 @@ class DiskMap:
     def __repr__(self):
         return ''.join(map(str,self.entries))
 
-    def last_file_idx(self) -> int:
+    def last_file_idx(self, start=-1) -> int:
         """obtain index to last file"""
-        for i in range(len(self.entries)-1, 0, -1):
+        if start < 0 or start >= len(self.entries):
+            start = len(self.entries)-1
+        for i in range(start, 0, -1):
             if self.entries[i].etype == "file":
                 return i
         return -1
 
-    def index_of_file(self, file_id: int) -> int:
+    def index_of_file(self, file_id: int, start=-1) -> int:
         """obtain index to last file"""
-        for i in range(len(self.entries)-1, 0, -1):
+        if start < 0 or start >= len(self.entries):
+            start = len(self.entries)-1
+        for i in range(start, 0, -1):
             if self.entries[i].etype == "file" and self.entries[i].file_id == file_id:
                 return i
         return -1
 
-    def first_free_idx(self) -> int:
+    def first_free_idx(self, start=0) -> int:
         """obtain index to first free space"""
-        for i, e in enumerate(self.entries):
-            if e.etype == "free":
+        for i in range(start, len(self.entries),1):
+            if self.entries[i].etype == "free":
                 return i
         return -1
 
@@ -99,14 +103,16 @@ class DiskMap:
                     self.entries[file_idx+1].blocks += free.blocks
                 else:
                     self.entries.insert(file_idx+1,FreeSpace(free.blocks))
-            file_idx = self.last_file_idx()
-            free_idx = self.first_free_idx()
+            file_idx = self.last_file_idx(file_idx+1)
+            free_idx = self.first_free_idx(free_idx)
 
     def defrag(self):
         """compress free space by moving complete files left"""
-        last_file = self.entries[self.last_file_idx()]
+        file_idx = self.last_file_idx()
+        last_file = self.entries[file_idx]
+        entry_count = len(self.entries)
         for file_id in range(last_file.file_id, -1, -1):
-            file_idx = self.index_of_file(file_id)
+            file_idx = self.index_of_file(file_id, file_idx)
             file = self.entries[file_idx]
             free_idx = self.first_free_idx()
             while free_idx < file_idx:
@@ -117,15 +123,17 @@ class DiskMap:
                     break
                 if file.blocks < free.blocks:
                     self.entries.insert(free_idx, file)
-                    if file_idx + 2 < len(self.entries) and self.entries[file_idx+2].etype == "free":
+                    entry_count += 1
+                    if file_idx + 2 < entry_count and self.entries[file_idx+2].etype == "free":
                         self.entries[file_idx+2].blocks += file.blocks
                         del self.entries[file_idx+1]
+                        entry_count -= 1
                     else:
                         self.entries[file_idx+1] = FreeSpace(file.blocks)
                     free.blocks -= file.blocks
                     break
                 free_idx += 1
-                while free_idx < len(self.entries):
+                while free_idx < entry_count:
                     if self.entries[free_idx].etype == "free":
                         break
                     free_idx += 1
