5aa26c19de8e1d1e4d1a6d52b2eda93206ed2dd3
scottdharris <scottdharris11@yahoo.com>
2024-12-09 08:24:21 -0500

    day 9, part 1 simplified but still wrong

--- day09.py
+++ day09.py
@@ -5,7 +5,7 @@ from utilities.runner import runner
 @runner("Day 9", "Part 1")
 def solve_part1(lines: list[str]):
     """part 1 solving function"""
-    diskmap = parse_diskmap(lines[0])
+    diskmap = DiskMap(lines[0])
     diskmap.compress()
     return diskmap.checksum()
 
@@ -33,76 +33,45 @@ class FreeSpace:
     
 class DiskMap:
     """diskmap definition"""
-    def __init__(self, entries: list) -> None:
-        self.entries = entries
+    def __init__(self, line: str) -> None:
+        self.blocks = ""
+        file = True
+        file_no = 0
+        for c in line:
+            blocks = int(c)
+            if file:
+                self.blocks += str(file_no) * blocks
+                file_no += 1
+            else:
+                self.blocks += "." * blocks
+            file = not file
 
     def __repr__(self):
-        return ''.join(map(str,self.entries))
-
-    def last_file_idx(self) -> int:
-        """obtain index to last file"""
-        for i in range(len(self.entries)-1, 0, -1):
-            if isinstance(self.entries[i], File):
-                return i
-        return -1
-
-    def first_free_idx(self) -> int:
-        """obtain index to first free space"""
-        for i, e in enumerate(self.entries):
-            if isinstance(e, FreeSpace):
-                return i
-        return -1
+        return self.blocks
 
     def compress(self):
         """compress files into leftmost free space"""
-        file_idx = self.last_file_idx()
-        free_idx = self.first_free_idx()
-        while file_idx >= 0 and free_idx >= 0 and file_idx > free_idx:
-            file = self.entries[file_idx]
-            free = self.entries[free_idx]
-            if file.blocks == free.blocks:
-                self.entries[free_idx] = file
-                self.entries[file_idx] = free
-            elif file.blocks < free.blocks:
-                self.entries.insert(free_idx, file)
-                if file_idx + 2 < len(self.entries) and isinstance(self.entries[file_idx+2], FreeSpace):
-                    self.entries[file_idx+2].blocks += file.blocks
-                    del self.entries[file_idx+1]
-                else:
-                    self.entries[file_idx+1] = FreeSpace(file.blocks)
-                free.blocks -= file.blocks
-            else:
-                self.entries[free_idx] = File(file.file_id, free.blocks)
-                file.blocks -= free.blocks
-                if file_idx + 1 < len(self.entries) and isinstance(self.entries[file_idx+1], FreeSpace):
-                    self.entries[file_idx+1].blocks += free.blocks
-                else:
-                    self.entries.insert(file_idx+1,FreeSpace(free.blocks))
-            file_idx = self.last_file_idx()
-            free_idx = self.first_free_idx()
+        work = list(self.blocks)
+        file_idx = len(self.blocks) - 1
+        free_idx = 0
+        while True:
+            while work[free_idx] != '.':
+                free_idx += 1
+            while work[file_idx] == '.':
+                file_idx -= 1
+            if file_idx < free_idx:
+                break
+            work[free_idx] = work[file_idx]
+            work[file_idx] = '.'
+        self.blocks = "".join(work)
 
     def checksum(self) -> int:
         """compute checksum for diskmap"""
         chk = 0
-        for i, c in enumerate(str(self)):
+        for i, c in enumerate(self.blocks):
             if c != '.':
                 chk += int(c) * i
         return chk
-
-def parse_diskmap(s: str) -> DiskMap:
-    """parse a diskmap from the supplied string"""
-    entries = []
-    file = True
-    file_no = 0
-    for c in s:
-        blocks = int(c)
-        if file:
-            entries.append(File(file_no, blocks))
-            file_no += 1
-        else:
-            entries.append(FreeSpace(blocks))
-        file = not file
-    return DiskMap(entries)
 
 # Data
 data = read_lines("input/day09/input.txt")
@@ -110,7 +79,7 @@ sample = """2333133121414131402""".splitlines()
 
 # Part 1
 assert solve_part1(sample) == 1928
-assert solve_part1(data) > 89093266248
+assert solve_part1(data) > 93714629328
 
 # Part 2
 assert solve_part2(sample) == 0
