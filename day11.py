7e23b38d6db723207633ba2a7dcdb13ab8870268
scottdharris <scottdharris11@yahoo.com>
2024-12-28 08:05:02 -0500

    small refactor

--- day11.py
+++ day11.py
@@ -6,7 +6,16 @@ from utilities.runner import runner
 @runner("Day 11", "Part 1")
 def solve_part1(values: list[str], blinks: int):
     """part 1 solving function"""
-    stones = dict(Counter(values))
+    return count_stones(values, blinks)
+
+@runner("Day 11", "Part 2")
+def solve_part2(values: list[str]):
+    """part 2 solving function"""
+    return count_stones(values, 75)
+
+def count_stones(initial: list[str], blinks: int):
+    """count the stones created after supplied number of blinks"""
+    stones = dict(Counter(initial))
 
     for _ in range(blinks):
         wstones = {}
@@ -36,5 +45,5 @@ assert solve_part1(sample, 6) == 22
 assert solve_part1(sample, 25) == 55312
 assert solve_part1(data, 25) == 220999
 
-# Part 2 (using part 1 for longer iteration)
-assert solve_part1(data, 75) == 261936432123724
+# Part 2
+assert solve_part2(data) == 261936432123724
